c5ad1bfe76c5607d36181d4c604e389761956060
yasmin <ypbernardo@gmail.com>
2017-07-31 19:46:53 -0300

    icones, data binding, splashscreen activity, manifest como a launch e cores

--- app/src/main/java/br/com/desafiolegbr/splashscreen/SplashscreenActivity.java
+++ app/src/main/java/br/com/desafiolegbr/splashscreen/SplashscreenActivity.java
@@ -1,10 +1,26 @@
 package br.com.desafiolegbr.splashscreen;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import br.com.desafiolegbr.R;
+import br.com.desafiolegbr.databinding.ActivitySplashscreenBinding;
 
 /**
  * Created by yasmin on 31/07/17.
  */
 
 public class SplashscreenActivity extends Activity {
+
+    ActivitySplashscreenBinding mBinding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_splashscreen);
+
+    }
 }
