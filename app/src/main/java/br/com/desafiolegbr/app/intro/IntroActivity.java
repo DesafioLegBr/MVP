2a5a6bb9870af556f35bd028ba4faa3b7a5c9b00
Matheus Sambiase <matheus.sambiase@concrete.com.br>
2017-08-07 11:48:00 -0300

    escondendo toolbar

--- app/src/main/java/br/com/desafiolegbr/app/intro/IntroActivity.java
+++ app/src/main/java/br/com/desafiolegbr/app/intro/IntroActivity.java
@@ -1,33 +1,31 @@
 package br.com.desafiolegbr.app.intro;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
-import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntro2;
 import com.github.paolorotolo.appintro.AppIntroFragment;
 
 import br.com.desafiolegbr.R;
 
-/**
- * Created by root-matheus on 31/07/17.
- */
 
 public class IntroActivity extends AppIntro2 {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setupToolbar();
         addSlide(AppIntroFragment.newInstance("DesafioLegBR", "Ensino na técnologia", R.drawable.splashscreen_desafio, Color.parseColor("#0097de")));
         addSlide(AppIntroFragment.newInstance("Um app com acessibilidade", "No mundo é necessário olhar pelo próximo." +
         "Buscamos isso atraves de uma grande acessibilidade para todos", R.drawable.img_acessibilidade, Color.parseColor("#FF4ABFED")));
 
     }
 
+    private void setupToolbar(){
+        getSupportActionBar().hide();
+    }
     @Override
     public void onSkipPressed(Fragment currentFragment) {
         super.onSkipPressed(currentFragment);
